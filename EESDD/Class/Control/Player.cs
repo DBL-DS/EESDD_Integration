f3dfe915032477db4e01d984c6ea8247e48834ec
silverHugh <hugh_77@icloud.com>
2016-07-28 11:23:54 +0800

    thread part

---
+++ EESDD/Class/Control/Player.cs
@@ -0,0 +1,31 @@
+﻿using EESDD.Class.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EESDD.Class.Control
+{
+    class Player
+    {
+        private Scene scene;
+        private Mode mode;
+
+        public void SelectScene(Scene scene)
+        {
+            this.scene = scene;
+        }
+
+        public void SelectMode(Mode mode)
+        {
+            this.mode = mode;
+        }
+
+        public bool PlayStart()
+        {
+            return false;
+        }
+
+    }
+}
--- EESDD/Class/Control/ThreadManager.cs
+++ EESDD/Class/Control/ThreadManager.cs
@@ -2,11 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EESDD.Class.Control
 {
-    class ThreadManager
+    static class ThreadManager
     {
+        static ThreadManager()
+        {
+            threads = new Dictionary<string, Thread>();
+        }
+
+        private static Dictionary<string, Thread> threads;
+
+        public static Thread CreateThread(string name, ThreadStart func)
+        {
+            Thread thread = new Thread(func);
+            thread.Name = name;
+            threads.Add(name, thread);
+
+            return thread;
+        }
+
+        public static bool StartThread(string threadName)
+        {
+            if (!threads.ContainsKey(threadName))
+                return false;
+
+            return StartThread(threads[threadName]);
+        }
+
+        public static bool StartThread(Thread thread)
+        {
+            thread.Start();
+            return true;
+        }
+
+        public static bool StartThread(string threadName, object para)
+        {
+            if (!threads.ContainsKey(threadName))
+                return false;
+
+            return StartThread(threads[threadName], para);
+        }
+
+        public static bool StartThread(Thread thread, object para)
+        {
+            thread.Start(para);
+            return true;
+        }
     }
 }
