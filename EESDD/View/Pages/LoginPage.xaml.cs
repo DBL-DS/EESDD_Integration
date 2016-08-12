8ef3eecd54035a9586890c6eca2cbc973eda5b32
silverHugh <hugh_77@icloud.com>
2016-08-13 00:04:27 +0800

    multibiding to center border in canvas

--- EESDD/View/Pages/LoginPage.xaml.cs
+++ EESDD/View/Pages/LoginPage.xaml.cs
@@ -25,6 +25,54 @@ namespace EESDD.View.Pages
             InitializeComponent();
         }
 
+        private void ShowRegularButton()
+        {
+            RegularButton.Visibility = System.Windows.Visibility.Visible;
+            AdminButton.Visibility = System.Windows.Visibility.Collapsed;
+        }
 
+        private void ShowAdminButton()
+        {
+            RegularButton.Visibility = System.Windows.Visibility.Collapsed;
+            AdminButton.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void ShowRegisterPanel()
+        {
+            LeftButton.Visibility = System.Windows.Visibility.Visible;
+            RightButton.Visibility = System.Windows.Visibility.Hidden;
+            LoginPanel.Visibility = System.Windows.Visibility.Hidden;
+            RegisterPanel.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void ShowLoginPanel()
+        {
+            LeftButton.Visibility = System.Windows.Visibility.Hidden;
+            RightButton.Visibility = System.Windows.Visibility.Visible;
+            LoginPanel.Visibility = System.Windows.Visibility.Visible;
+            RegisterPanel.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        # region Event
+        private void RightButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowRegisterPanel();
+        }
+
+        private void LeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowLoginPanel();
+        }
+
+        private void RegularButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowAdminButton();
+        }
+
+        private void AdminButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowRegularButton();
+        }
+        # endregion
     }
 }
---
+++ EESDD/View/Style/HalfValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EESDD.View.Style
+{
+    public class HalfValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            double parentLength = (double)values[0];
+            double length = (double)values[1];
+
+            return (parentLength - length) / 2;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes,
+	        object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
